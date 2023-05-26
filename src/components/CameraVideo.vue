9b17f29cd88cee86239a582e81a11e428df0dc6f
zhangmx <icediv@163.com>
2023-05-26 17:49:55 +0800

    tmp commit

--- src/components/CameraVideo.vue
+++ src/components/CameraVideo.vue
@@ -1,20 +1,44 @@
 <template>
   <q-card dark bordered class="bg-grey-9 my-card" v-if="enabled">
-      <q-card-section>
-        <div class="text-h6">{{ label }}</div>
-        <div class="text-subtitle2">{{ label }}</div>
-      </q-card-section>
+    <q-card-section>
 
-      <q-separator dark inset />
+      <video ref="videoPlayerElement" class="video-js" />
 
-      <q-card-section>
-        {{ savedFolder }}
-      </q-card-section>
+      <div class="text-h6">{{ label }}</div>
+      <div class="text-subtitle2">{{ label }}</div>
+    </q-card-section>
+
+    <q-separator dark inset />
+
+    <q-card-section>
+      {{ savedFolder }}
+    </q-card-section>
   </q-card>
 </template>
 
 <script>
 import { defineComponent } from 'vue';
+import videojs from 'video.js';
+
+const options = {
+  controls: true,
+  width: 320,
+  height: 240,
+  fluid: false,
+  bigPlayButton: false,
+  controlBar: {
+    volumePanel: false
+  },
+  plugins: {
+    record: {
+      audio: false,
+      video: true,
+      maxLength: 10,
+      displayMilliseconds: false,
+      debug: true
+    }
+  }
+};
 
 export default defineComponent({
   name: 'CameraLink',
@@ -67,10 +91,27 @@ export default defineComponent({
     },
   },
 
-  setup(props) {
+  setup(props, context) {
     console.log('CameraLink', props.savedFolder)
 
     const link = 'https://www.youtube.com/embed/k3_tw44QsZQ?rel=0'
+
+    const videoPlayer = videojs(context.refs.videoPlayerElement, options, () => {
+      // this.player.log("onPlayerReady", this);
+    });
+
+    // videoPlayer.src({
+    //   src: link,
+    //   type: 'application/x-mpegURL'
+    // });
+    async function getCameraStream() {
+      const stream = await navigator.mediaDevices.getUserMedia({
+        video: { deviceId: { exact: props.deviceId } },
+      });
+      videoPlayer.srcObject = stream;
+    }
+
+    getCameraStream()
 
     return {
       link
---
+++ src/components/CameraVideo1.vue
@@ -0,0 +1,15 @@
+<template>
+
+      <video ref="videoPlayer" class="video-js" />
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'CameraLink',
+  setup(props, context) {
+
+  }
+});
+</script>
